249a5da167bb4a59047e219f654321ef0637a49e
mhkim <nodark0203@gmail.com>
2023-07-19 17:58:26 +0900

    datetimeoffset offset 변경 테스트

---
+++ TestProject1/DateTimeOffsetTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1;
+
+public class DateTimeOffsetTest
+{
+    [Test]
+    public void DateTimeOffset_변경테스트()
+    {
+        DateTimeOffset dt = DateTimeOffset.UtcNow;
+        Console.WriteLine(dt);
+        Console.WriteLine(dt.Offset);
+        Console.WriteLine(dt.UtcDateTime);
+        Console.WriteLine(dt.ToLocalTime());
+
+        DateTimeOffset dt2 = dt;
+        dt2 = dt2.ToOffset(new TimeSpan(8, 0, 0));
+        Console.WriteLine(dt2);
+    }
+}
