695ad3d2739294baf6546ce3067dc57e48e3c32f
mhkim <nodark0203@gmail.com>
2023-08-17 09:59:34 +0900

    test

---
+++ TestProject1/LinqTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using TestProject1.Db;
+
+namespace TestProject1;
+
+public class LinqTest
+{
+    public List<LinqTestData> TestData { get; set; } =  new List<LinqTestData>();
+    public int[] datas = new int[] { 1, 2, 3, 4, 5 };
+
+    [SetUp]
+    public void Setup()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            TestData.Add(new LinqTestData() { Id = $"ID-{i}", index = i });
+        }
+    }
+
+
+    [Test]
+    public Task FindTest()
+    {
+        int findData = Array.Find(datas, x => x == 10);
+        Console.WriteLine(findData);
+
+        return Task.CompletedTask;
+    }
+
+    [Test]
+    public Task FindClassTest()
+    {
+        LinqTestData? findData = TestData.Find(x => x.index == 11);
+        if(findData != null)
+        {
+            Console.WriteLine(findData.ToString());
+        }
+
+        return Task.CompletedTask;
+    }
+}
+
+public class LinqTestData
+{
+    public string Id { get; set; } = string.Empty;
+    public int index { get; set; }
+}
---
+++ TestProject1/StringTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1;
+
+public class StringTest
+{
+    [Test]
+    public void EnumTostringTest()
+    {
+        Console.WriteLine(Codes.None);
+        Console.WriteLine(Codes.Success);
+        Console.WriteLine(Codes.Fail);
+
+        Console.WriteLine($"{Codes.None}");
+        Console.WriteLine($"{Codes.Success}");
+        Console.WriteLine($"{Codes.Fail}");
+
+        Console.WriteLine($"{Codes.None.ToString()}");
+        Console.WriteLine($"{Codes.Success.ToString()}");
+        Console.WriteLine($"{Codes.Fail.ToString()}");
+    }
+
+
+    public enum Codes
+    {
+        None = 0,
+        Success = 1,
+        Fail = 2
+    }
+}
